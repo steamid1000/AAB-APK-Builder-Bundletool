40a46119a16b5bdae9b32f7d9ac3f887e9ed24a3
Shivrudra Patil <steamid1000@gmail.com>
2025-11-08 17:06:16 +0530

    Implemented the commandline arguments
    Now you can pass all the arguments to the program or partial arguments as well, in that case you will be asked to enter the remaining arguments in the terminal.
    
    Note: Do follow the argument order, or there will be errors.
    The arguments are defined in the readme file.

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ A small console program that makes it easy to build apk files from AAB bundle fi
 ## Note: The program requires 7zip to function
 
 ## TODOs:
-    1. Add command line arguments, so that we run this program with other tools just passing the required arguments.
+    1. Add command line arguments, so that we run this program with other tools just passing the required arguments : ✔ 
 
 You can compile the program and pass it the path of the aab file and it will create the universal-apk from that aab file using the bundletool
 This is a automation tool and will see many updates to add new features down the line.
+
+## CMD arguments passing order
+    BundlePath -> KeyStorePath -> StorePass -> KeyAlias -> AliasPassword
 
 Below is the snap of a trial build
 ![image](https://github.com/steamid1000/AAB-APK-Builder-Bundletool/assets/73237099/7ac14d54-baf5-4f5c-a703-c0d0af58e175)
--- core.cpp
+++ core.cpp
@@ -12,42 +12,45 @@
 const std::string bundleToolFilePath = "resources/bundletool-all-1.15.6.jar";
 const std::string tempBundlefileName = "Temp-out.apks";
 
-
-class Spinner{
+class Spinner
+{
     std::atomic<bool> running{true};
     std::thread spinnerThread;
 
-    void spin(){
-        const char* spinners = "|/-\\";
+    void spin()
+    {
+        const char *spinners = "|/-\\";
         int i = 0;
 
-        while(running){
-            std::cout<<"\r"<<spinners[i++ % 4]<< "Processing...";
+        while (running)
+        {
+            std::cout << "\r" << spinners[i++ % 4] << "Processing...";
             std::cout.flush();
             std::this_thread::sleep_for(std::chrono::milliseconds(250));
         }
 
-
         // clear the loading spinner
-        std::cout<<"\r"<<std::string(30,' ')<<"\r";
+        std::cout << "\r" << std::string(30, ' ') << "\r";
     }
 
-    public:
+public:
     void start()
     {
         running = true;
-        spinnerThread = std::thread(&Spinner::spin,this);
+        spinnerThread = std::thread(&Spinner::spin, this);
     }
 
-    void stop(){
+    void stop()
+    {
         running = false;
-        if(spinnerThread.joinable()){
+        if (spinnerThread.joinable())
+        {
             spinnerThread.join();
         }
     }
 };
 
-std::string fileName(std::string bundleFilePath) 
+std::string fileName(std::string bundleFilePath)
 // This function return the name of the aab file so that we can use it for the final apk
 {
     // We will find the . in the .aab file path name and go backwards until be encounter a '/' or a '\' or a '\\'
@@ -69,26 +72,40 @@ std::string fileName(std::string bundleFilePath)
 void takePathsandKeys(std::string &bundleFilePath, std::string &keystoreFilePath, std::string &keyPass, std::string &keyAlias, std::string &aliasPassword, std::string &finalExtractName)
 {
     // get the aab file path
-    printf("Paste the .aab file path along with its name: \n");
-    std::cin >> bundleFilePath;
+    if (bundleFilePath.empty())
+    {
+        printf("Paste the .aab file path along with its name: \n");
+        std::cin >> bundleFilePath;
+    }
 
-    // get the keystore file path otherwise we will fall back to the default keystore
-    printf("Past the keystore path for signing this apk(or type no to use the default keystore): \n");
-    std::cin >> keystoreFilePath;
+    if (keystoreFilePath.empty())
+    {
+        // get the keystore file path otherwise we will fall back to the default keystore
+        printf("Past the keystore path for signing this apk(or type no to use the default keystore): \n");
+        std::cin >> keystoreFilePath;
+    }
 
     // extract the name of the aab file using the method above
     finalExtractName = fileName(bundleFilePath);
 
     // get the passwords of the keystores
-    printf("Enter the keystore password: \n");
-    std::cin>>keyPass;
+    if (keyPass.empty())
+    {
+        printf("Enter the keystore password: \n");
+        std::cin >> keyPass;
+    }
 
-    printf("Enter the key alias name : \n");
-    std::cin>>keyAlias;
+    if (keyAlias.empty())
+    {
+        printf("Enter the key alias name : \n");
+        std::cin >> keyAlias;
+    }
 
-    printf("Enter the alias password: \n");
-    std::cin>>aliasPassword;
-
+    if (aliasPassword.empty())
+    {
+        printf("Enter the alias password: \n");
+        std::cin >> aliasPassword;
+    }
 }
 
 std::string makeZipBuildCommand(const std::string &bundleFilePath, const std::string &keystoreFilePath, const std::string &keyPass, const std::string &keyAlias, const std::string &aliasPassword, const std::string &tempBundlefileName, const std::string &dotApks)
@@ -97,42 +114,40 @@ std::string makeZipBuildCommand(const std::string &bundleFilePath, const std::st
     // java -jar bundletool-all-1.15.6.jar build-apks --bundle=path\to\app.aab --output=path\to\output.apks --ks=path\to\keystore.jks --ks-pass=pass:your_keystore_password --ks-key-alias=your_key_alias --key-pass=pass:your_key_password --mode=universal
 
     // Now we have all the required variables,lets build the command
-    return "java -jar " + bundleToolFilePath + " build-apks --mode=universal --bundle=" 
-    + bundleFilePath + " --output=" + tempBundlefileName + dotApks + " --ks=" + keystoreFilePath + " --ks-key-alias=" + 
-    keyAlias+ " --ks-pass=pass:" + keyPass+ " --key-pass=pass:" + aliasPassword;
-
-
+    return "java -jar " + bundleToolFilePath + " build-apks --mode=universal --bundle=" + bundleFilePath + " --output=" + tempBundlefileName + dotApks + " --ks=" + keystoreFilePath + " --ks-key-alias=" +
+           keyAlias + " --ks-pass=pass:" + keyPass + " --key-pass=pass:" + aliasPassword;
 }
 
-void doPostProcessing(std::string &finalExtractName, const std::string &tempBundlefileName, const std::string &dotApks, const std::string &dotzip){
-        printf("\n\n\t\tZip Built..\n");
+void doPostProcessing(std::string &finalExtractName, const std::string &tempBundlefileName, const std::string &dotApks, const std::string &dotzip)
+{
+    printf("\n\n\t\tZip Built..\n");
 
-        // Lets rename it
-        std::string rename = "rename " + tempBundlefileName + dotApks + " " + tempBundlefileName + dotzip;
-        const char *renameCommand = rename.c_str();
-        system(renameCommand);
-        printf("\t\tFile Renamed Successfully..\n");
+    // Lets rename it
+    std::string rename = "rename " + tempBundlefileName + dotApks + " " + tempBundlefileName + dotzip;
+    const char *renameCommand = rename.c_str();
+    system(renameCommand);
+    printf("\t\tFile Renamed Successfully..\n");
 
-        // Now Lets Extract the apk file from the zip file using 7zip cmd
-        std::string extract = "7z x " + tempBundlefileName + dotzip + " *.apk";
-        const char *extractCommand = extract.c_str();
-        system(extractCommand);
-        printf("\t\tApk Extracted..\n");
+    // Now Lets Extract the apk file from the zip file using 7zip cmd
+    std::string extract = "7z x " + tempBundlefileName + dotzip + " *.apk";
+    const char *extractCommand = extract.c_str();
+    system(extractCommand);
+    printf("\t\tApk Extracted..\n");
 
-        // Now lets remove the temp zip file as we no longer need it
-        std::string deleteTemp = "del " + tempBundlefileName + dotzip;
-        const char *deleteTempCommand = deleteTemp.c_str();
-        system(deleteTempCommand);
-        printf("\t\tDeleted Temp files..\n");
+    // Now lets remove the temp zip file as we no longer need it
+    std::string deleteTemp = "del " + tempBundlefileName + dotzip;
+    const char *deleteTempCommand = deleteTemp.c_str();
+    system(deleteTempCommand);
+    printf("\t\tDeleted Temp files..\n");
 
-        // Now lets rename the apk to the .aab file name
-        rename = "rename Universal.apk " + finalExtractName + ".apk";
-        const char *apkRename = rename.c_str();
-        system(apkRename);
+    // Now lets rename the apk to the .aab file name
+    rename = "rename Universal.apk " + finalExtractName + ".apk";
+    const char *apkRename = rename.c_str();
+    system(apkRename);
 
-        printf("\t\tApk File Renamed..\n");
+    printf("\t\tApk File Renamed..\n");
 
-        printf("\t\tProcess: Success\n");
+    printf("\t\tProcess: Success\n");
 }
 
 int main(int argc, char *argv[])
@@ -147,7 +162,7 @@ int main(int argc, char *argv[])
 
     // spinner
     Spinner loadSpinner;
-    
+
     std::string bundleFilePath;
     int buildResult = -1;
 
@@ -162,28 +177,66 @@ int main(int argc, char *argv[])
     std::string dotApks = ".apks";
     std::string dotzip = ".zip";
 
+    // check which values were passed as arguments to the program
+    // positions: bundlePath -> keystorePath -> keyStorePass -> storeAlias -> aliasPass
+    if (argc > 1 && argc == 6)
+    {
+        bundleFilePath = argv[1];
+        keystoreFilePath = argv[2];
+        keyPass = argv[3];
+        keyAlias = argv[4];
+        aliasPassword = argv[5];
+    }
+    else if (argc > 1 && argc <= 6) // this means that some of the arugments were missing, so we need to take input of them
+    {
+        for (size_t i = 1; i < argc; i++)
+        {
+            switch (i)
+            {
+            case 1:
+                bundleFilePath = argv[i];
+                break;
+            case 2:
+                keystoreFilePath = argv[i];
+                break;
+            case 3:
+                keyPass = argv[i];
+                break;
+            case 4:
+                keyAlias = argv[i];
+                break;
+            case 5:
+                aliasPassword = argv[i];
+                break;
+            default:
+                break;
+            }
+        }
+
+        printf("\n Some arguments are still missing, fill them out.\n");
+    }
+
     // take the required paths and passwords from the user
     takePathsandKeys(bundleFilePath, keystoreFilePath, keyPass, keyAlias, aliasPassword, finalExtractName);
-    
+
     // make the zip build command
     std::string buildCommand = makeZipBuildCommand(bundleFilePath, keystoreFilePath, keyPass, keyAlias, aliasPassword, tempBundlefileName, dotApks);
 
     // run this command, the sytem function requires a const char*
-    std:: future<int> buildResultFuture = std::async(std::launch::async,[&](){
+    std::future<int> buildResultFuture = std::async(std::launch::async, [&]()
+                                                    {
         const char *finalbuildCommand = buildCommand.c_str();
         finalbuildCommand; // here we are making a apk zip file
-        return system(finalbuildCommand);
-    });
+        return system(finalbuildCommand); });
 
     loadSpinner.start();
     buildResult = buildResultFuture.get();
     loadSpinner.stop();
 
-
     // check if the command was successful
     if (buildResult == 0)
     {
-       doPostProcessing(finalExtractName, tempBundlefileName, dotApks, dotzip); 
+        doPostProcessing(finalExtractName, tempBundlefileName, dotApks, dotzip);
     }
     else
     {
